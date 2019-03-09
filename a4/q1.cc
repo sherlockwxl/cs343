527b3b6d3bc6305232245ca8af31408b442d5839
xilingwu <wuxiling@outlook.com>
2019-03-09 02:48:25 -0500

    a4 ini

---
+++ a4/q1.cc
@@ -0,0 +1,47 @@
+#if defined( MC )                    // mutex/condition solution
+// includes for this kind of vote-tallier
+class TallyVotes {
+    // private declarations for this kind of vote-tallier
+#elif defined( SEM )                // semaphore solution
+// includes for this kind of vote-tallier
+class TallyVotes {
+    // private declarations for this kind of vote-tallier
+#elif defined( BAR )                // barrier solution
+// includes for this kind of vote-tallier
+_Cormonitor TallyVotes : public uBarrier {
+    // private declarations for this kind of vote-tallier
+#else
+    #error unsupported voter type
+#endif
+    // common declarations
+  public:                            // common interface
+    _Event Failed {};
+    TallyVotes( unsigned int voters, unsigned int group, Printer & printer );
+    struct Ballot { unsigned int picture, statue, giftshop; };
+    enum TourKind { Picture = 'p', Statue = 's', GiftShop = 'g' };
+    struct Tour { TourKind tourkind; unsigned int groupno; };
+    Tour vote( unsigned int id, Ballot ballot );
+    void done();
+};
+
+_Task Voter {
+    TallyVotes::Ballot cast() {        // cast 3-way vote
+        // O(1) random selection of 3 items without replacement using divide and conquer.
+        static const unsigned int voting[3][2][2] = { { {2,1}, {1,2} }, { {0,2}, {2,0} }, { {0,1}, {1,0} } };
+        unsigned int picture = mprng( 2 ), statue = mprng( 1 );
+        return (TallyVotes::Ballot){ picture, voting[picture][statue][0], voting[picture][statue][1] };
+    }
+  public:
+    enum States { Start = 'S', Vote = 'V', Block = 'B', Unblock = 'U', Barging = 'b',
+                   Complete = 'C', Finished = 'F', Failed = 'X', Terminated = 'T' };
+    Voter( unsigned int id, unsigned int nvotes, TallyVotes & voteTallier, Printer & printer );
+};
+
+_Monitor / _Cormonitor Printer {    // chose one of the two kinds of type constructor
+  public:
+    Printer( unsigned int voters );
+    void print( unsigned int id, Voter::States state );
+    void print( unsigned int id, Voter::States state, TallyVotes::Tour tour );
+    void print( unsigned int id, Voter::States state, TallyVotes::Ballot ballot );
+    void print( unsigned int id, Voter::States state, unsigned int numBlocked );
+};
